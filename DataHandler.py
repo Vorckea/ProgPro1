ebc507b6b632af841bd3a079e63ab9163dcc966e
EliasT05 <46528691+EliasT05@users.noreply.github.com>
2022-12-06 22:52:56 +0100

    Average to mean in datahandler, basic scatterplot

--- DataHandler.py
+++ DataHandler.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def fixTable(df: pd.DataFrame) -> pd.DataFrame:
     attributes = df["elementId"].tolist()
-    return df.pivot_table(index="referenceTime", columns="elementId", values="value", aggfunc="average")
+    return df.pivot_table(index="referenceTime", columns="elementId", values="value", aggfunc="mean")
