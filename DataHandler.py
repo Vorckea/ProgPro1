bbc057f3e0367e8ae0d0c49461d4cf985e5a812d
Vorckea <akselschrader@gmail.com>
2022-12-07 17:27:13 +0100

    Eff update

--- DataHandler.py
+++ DataHandler.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def fixTable(df: pd.DataFrame) -> pd.DataFrame:
-    attributes = df["elementId"].tolist()
     df = df.pivot_table(index="referenceTime", columns="elementId", values="value", aggfunc="mean")
     df = df.reset_index()
     return df
