b5c9e74ddbf555680d917ed8f4ba3a40d671d640
Vorckea <akselschrader@gmail.com>
2022-12-07 17:30:32 +0100

    Mer eff

--- DataHandler.py
+++ DataHandler.py
@@ -1,7 +1,14 @@
 import pandas as pd
+from os.path import exists
 
 def fixTable(df: pd.DataFrame) -> pd.DataFrame:
+    if(exists("dataframeFixed.csv")):
+        print("Found dataframeFixed.csv")
+        df = pd.read_csv("dataframeFixed.csv")
+        return df
+    
     df = df.pivot_table(index="referenceTime", columns="elementId", values="value", aggfunc="mean")
     df = df.reset_index()
+    df.to_csv("dataframeFixed.csv")
     return df
 
