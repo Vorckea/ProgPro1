ef1070ebe33d0468e133c128f866f932a137fe7f
EliasT05 <46528691+EliasT05@users.noreply.github.com>
2022-12-13 17:45:33 +0100

    Removed altair

--- Reqs.py
+++ Reqs.py
@@ -3,10 +3,6 @@ import subprocess
 def downloadReqs():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
     'requests'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'altair'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'altair_viewer'])
     
 if __name__=="__main__":
     downloadReqs()
